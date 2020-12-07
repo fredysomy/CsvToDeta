442d86146a29bc7eec843b2cc549847dab932523
Sean Beck <seanmckaybeck@gmail.com>
2020-12-07 10:02:51 -0500

    Update cli.py
    * Don't overload built-ins (id)
    * Use f-strings
    * Use type-hints for clarity
    * Document your code
    * Follow pep8 spacing
    * Skip unnecessary step of putting dicts into list then iterating the list

--- src/cli.py
+++ src/cli.py
@@ -1,26 +1,35 @@
-import fire
 import csv
-from deta import Deta
 
-def csvtodeta(id,path,db):
-    deta = Deta(id)
+import deta
+import fire
+
+
+def csv_to_deta(deta_id: str, path: str, db: str):
+    """
+    Reads in a CSV file and uploads its contents to a Deta Base
+    :param str deta_id: The ID of your target Deta Base instance
+    :param str path: The absolute path to the target CSV file
+    :param str db: The name of your Deta Base
+    :return: None
+    """
+    deta = deta.Deta(deta_id)
     datafile = deta.Base(db)
-    print("Reading data from {}".format(path))
-    arr=[]
-    with open (path) as csvFile:
-        csvReader = csv.DictReader(csvFile)
-        for csvRow in csvReader:
-           arr.append(csvRow)
-    for each in arr:
-        datafile.insert(each)
-        print("Uploaded {}".format(each))
-    print("succesfully uploaded {} data to {} Base".format(len(arr),db))
+    print(f"Reading CSV from {path}")
+    with open(path) as csv_file:
+        count = 0
+        csv_reader = csv.DictReader(csv_file)
+        for csv_row in csv_reader:
+            datafile.insert(csv_row)
+            print(f"Uploaded {csv_row}")
+            count += 1
+        print(f"Succesfully uploaded {count} rows to {db} Deta Base")
+
 
 def main():
     fire.Fire({
-        "csvtodeta":csvtodeta
+        "csvtodeta":csv_to_deta
     })
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
